38ee7e73a1eadd54dd2ee242db6c1f56803fdde0
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-02-23 23:56:11 -0300

    24. GroupBox - Container - Parte 1

---
+++ Solucao03/Win03_B/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Win03_B {
+    partial class Form1 {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.gbContrato = new System.Windows.Forms.GroupBox();
+            this.gbLinguagem = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.lblTipoContrato = new System.Windows.Forms.Label();
+            this.gbContrato.SuspendLayout();
+            this.gbLinguagem.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbContrato
+            // 
+            this.gbContrato.Controls.Add(this.radioButton3);
+            this.gbContrato.Controls.Add(this.radioButton2);
+            this.gbContrato.Controls.Add(this.radioButton1);
+            this.gbContrato.Location = new System.Drawing.Point(13, 13);
+            this.gbContrato.Name = "gbContrato";
+            this.gbContrato.Size = new System.Drawing.Size(447, 100);
+            this.gbContrato.TabIndex = 0;
+            this.gbContrato.TabStop = false;
+            this.gbContrato.Text = "Tipo de contrato desejado:";
+            // 
+            // gbLinguagem
+            // 
+            this.gbLinguagem.Controls.Add(this.checkBox8);
+            this.gbLinguagem.Controls.Add(this.checkBox7);
+            this.gbLinguagem.Controls.Add(this.checkBox6);
+            this.gbLinguagem.Controls.Add(this.checkBox5);
+            this.gbLinguagem.Controls.Add(this.checkBox4);
+            this.gbLinguagem.Controls.Add(this.checkBox3);
+            this.gbLinguagem.Controls.Add(this.checkBox2);
+            this.gbLinguagem.Controls.Add(this.checkBox1);
+            this.gbLinguagem.Location = new System.Drawing.Point(13, 120);
+            this.gbLinguagem.Name = "gbLinguagem";
+            this.gbLinguagem.Size = new System.Drawing.Size(447, 100);
+            this.gbLinguagem.TabIndex = 1;
+            this.gbLinguagem.TabStop = false;
+            this.gbLinguagem.Text = "Linguagens:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "PHP";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(83, 20);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(40, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "C#";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(201, 43);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(45, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "CLT";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(343, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(37, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "PJ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(31, 43);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(73, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Autônomo";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(151, 20);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(40, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "VB";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(219, 20);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(51, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Ruby";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(7, 52);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(40, 17);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Go";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(146, 52);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(53, 17);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.Text = "Cobol";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(67, 52);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(59, 17);
+            this.checkBox7.TabIndex = 6;
+            this.checkBox7.Text = "Python";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(219, 52);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(49, 17);
+            this.checkBox8.TabIndex = 7;
+            this.checkBox8.Text = "Java";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // lblTipoContrato
+            // 
+            this.lblTipoContrato.AutoSize = true;
+            this.lblTipoContrato.Location = new System.Drawing.Point(522, 56);
+            this.lblTipoContrato.Name = "lblTipoContrato";
+            this.lblTipoContrato.Size = new System.Drawing.Size(35, 13);
+            this.lblTipoContrato.TabIndex = 2;
+            this.lblTipoContrato.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 397);
+            this.Controls.Add(this.lblTipoContrato);
+            this.Controls.Add(this.gbLinguagem);
+            this.Controls.Add(this.gbContrato);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbContrato.ResumeLayout(false);
+            this.gbContrato.PerformLayout();
+            this.gbLinguagem.ResumeLayout(false);
+            this.gbLinguagem.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbContrato;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox gbLinguagem;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.Label lblTipoContrato;
+    }
+}
+
---
+++ Solucao03/Win03_B/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win03_B {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            foreach (RadioButton radio in gbContrato.Controls) {
+                radio.CheckedChanged += delegate (object sender, EventArgs args) {
+                    lblTipoContrato.Text = ((RadioButton)sender).Text;
+                };
+            }
+            //gbLinguagem.Controls
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e) {
+
+        }
+    }
+}
