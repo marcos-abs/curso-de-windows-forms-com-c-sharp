eaf502e11d682272da79780e01a5020373955f85
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-02-26 17:02:11 -0300

    40. WebBrowser e ProgressBar (com erro ainda, falta corrigir)

---
+++ Solucao04/Win14/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Win14 {
+    partial class Form1 {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnAcessar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(13, 62);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ScriptErrorsSuppressed = true;
+            this.webBrowser.Size = new System.Drawing.Size(770, 250);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.ProgressChanged += new System.Windows.Forms.WebBrowserProgressChangedEventHandler(this.webBrowser_ProgressChanged);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 328);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(100, 23);
+            this.progressBar.TabIndex = 1;
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(13, 13);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(689, 20);
+            this.txtURL.TabIndex = 2;
+            // 
+            // btnAcessar
+            // 
+            this.btnAcessar.Location = new System.Drawing.Point(708, 9);
+            this.btnAcessar.Name = "btnAcessar";
+            this.btnAcessar.Size = new System.Drawing.Size(75, 23);
+            this.btnAcessar.TabIndex = 3;
+            this.btnAcessar.Text = "Acessar";
+            this.btnAcessar.UseVisualStyleBackColor = true;
+            this.btnAcessar.Click += new System.EventHandler(this.btnAcessar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 430);
+            this.Controls.Add(this.btnAcessar);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.webBrowser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnAcessar;
+    }
+}
+
---
+++ Solucao04/Win14/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win14 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnAcessar_Click(object sender, EventArgs e) {
+            webBrowser.Url = new Uri(txtURL.Text);
+        }
+
+        private void webBrowser_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e) {
+            progressBar.Value = (int)e.CurrentProgress; // Erro valor superior 100 (150)
+        }
+    }
+}
