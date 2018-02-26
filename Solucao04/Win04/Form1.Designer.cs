58bc030d868483f22fb21792b54ce924eb72a18d
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-02-26 09:48:23 -0300

    31. CheckedListBox

--- Solucao04/Win04/Form1.Designer.cs
+++ Solucao04/Win04/Form1.Designer.cs
@@ -31,12 +31,12 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Font = new System.Drawing.Font("Kaufmann BT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("MankSans", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.OrangeRed;
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
             this.label1.Padding = new System.Windows.Forms.Padding(10);
-            this.label1.Size = new System.Drawing.Size(116, 65);
+            this.label1.Size = new System.Drawing.Size(133, 66);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
---
+++ Solucao04/Win05/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Win05 {
+    partial class Form1 {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.QuantidadeMarcados = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "PHP",
+            "Java",
+            "JavaScript",
+            "C#",
+            "VB",
+            "C++",
+            "C",
+            "GO",
+            "Ruby",
+            "Python"});
+            this.checkedListBox1.Location = new System.Drawing.Point(13, 13);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 169);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // QuantidadeMarcados
+            // 
+            this.QuantidadeMarcados.AutoSize = true;
+            this.QuantidadeMarcados.Location = new System.Drawing.Point(155, 39);
+            this.QuantidadeMarcados.Name = "QuantidadeMarcados";
+            this.QuantidadeMarcados.Size = new System.Drawing.Size(35, 13);
+            this.QuantidadeMarcados.TabIndex = 1;
+            this.QuantidadeMarcados.Text = "label1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(158, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 378);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.QuantidadeMarcados);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label QuantidadeMarcados;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
