f2751d48c3ecd1247cf6776018edd3f86f677474
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-03-01 17:20:13 -0300

    50. WinForms - Design - Menu e Layout - Parte 1.

--- Solucao05/Certweb/Form1.Designer.cs
+++ Solucao05/Certweb/Form1.Designer.cs
@@ -23,12 +23,141 @@
         /// o conteúdo deste método com o editor de código.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnPainel = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnTarefas = new System.Windows.Forms.Button();
+            this.btnLinks = new System.Windows.Forms.Button();
+            this.btnSobre = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Purple;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 15);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.panel2.Controls.Add(this.btnSobre);
+            this.panel2.Controls.Add(this.btnLinks);
+            this.panel2.Controls.Add(this.btnTarefas);
+            this.panel2.Controls.Add(this.btnPainel);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 15);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(270, 741);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnPainel
+            // 
+            this.btnPainel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.btnPainel.FlatAppearance.BorderSize = 0;
+            this.btnPainel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPainel.Font = new System.Drawing.Font("Candara", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPainel.ForeColor = System.Drawing.Color.White;
+            this.btnPainel.Image = ((System.Drawing.Image)(resources.GetObject("btnPainel.Image")));
+            this.btnPainel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPainel.Location = new System.Drawing.Point(46, 286);
+            this.btnPainel.Name = "btnPainel";
+            this.btnPainel.Size = new System.Drawing.Size(183, 70);
+            this.btnPainel.TabIndex = 1;
+            this.btnPainel.Text = "      Painel";
+            this.btnPainel.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(46, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(183, 210);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnTarefas
+            // 
+            this.btnTarefas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.btnTarefas.FlatAppearance.BorderSize = 0;
+            this.btnTarefas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTarefas.Font = new System.Drawing.Font("Candara", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTarefas.ForeColor = System.Drawing.Color.White;
+            this.btnTarefas.Image = ((System.Drawing.Image)(resources.GetObject("btnTarefas.Image")));
+            this.btnTarefas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTarefas.Location = new System.Drawing.Point(46, 391);
+            this.btnTarefas.Name = "btnTarefas";
+            this.btnTarefas.Size = new System.Drawing.Size(183, 70);
+            this.btnTarefas.TabIndex = 2;
+            this.btnTarefas.Text = "        Tarefas";
+            this.btnTarefas.UseVisualStyleBackColor = false;
+            // 
+            // btnLinks
+            // 
+            this.btnLinks.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.btnLinks.FlatAppearance.BorderSize = 0;
+            this.btnLinks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLinks.Font = new System.Drawing.Font("Candara", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLinks.ForeColor = System.Drawing.Color.White;
+            this.btnLinks.Image = ((System.Drawing.Image)(resources.GetObject("btnLinks.Image")));
+            this.btnLinks.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLinks.Location = new System.Drawing.Point(46, 496);
+            this.btnLinks.Name = "btnLinks";
+            this.btnLinks.Size = new System.Drawing.Size(183, 70);
+            this.btnLinks.TabIndex = 3;
+            this.btnLinks.Text = "     Links";
+            this.btnLinks.UseVisualStyleBackColor = false;
+            // 
+            // btnSobre
+            // 
+            this.btnSobre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.btnSobre.FlatAppearance.BorderSize = 0;
+            this.btnSobre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSobre.Font = new System.Drawing.Font("Candara", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSobre.ForeColor = System.Drawing.Color.White;
+            this.btnSobre.Image = ((System.Drawing.Image)(resources.GetObject("btnSobre.Image")));
+            this.btnSobre.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSobre.Location = new System.Drawing.Point(46, 601);
+            this.btnSobre.Name = "btnSobre";
+            this.btnSobre.Size = new System.Drawing.Size(183, 70);
+            this.btnSobre.TabIndex = 4;
+            this.btnSobre.Text = "      Sobre";
+            this.btnSobre.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 756);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnPainel;
+        private System.Windows.Forms.Button btnSobre;
+        private System.Windows.Forms.Button btnLinks;
+        private System.Windows.Forms.Button btnTarefas;
     }
 }
 
