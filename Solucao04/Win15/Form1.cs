edc0b702a0558069d9be2e9ba01878a6e6c2ac9c
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-03-01 07:26:46 -0300

    41. WebBrowser - Renderizar HTML String.

---
+++ Solucao04/Win15/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win15 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            webBrowser1.DocumentText = "Olá Mundo <strong>HTML</strong><br/><br/><b>Este texto é negrito</b> este não é negrito.";
+        }
+    }
+}
