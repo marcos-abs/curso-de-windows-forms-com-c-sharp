a637cba4d4a279c5acc3200ad435be06a709724e
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-04-11 10:49:03 -0300

    67. WinForms - Programação - Centralizar texto em resource.

--- Solucao05/Certweb/Links.cs
+++ Solucao05/Certweb/Links.cs
@@ -12,16 +12,25 @@ using Certweb.Armazenamento.Modelo;
 
 namespace Certweb {
     public partial class Links : UserControl {
+
+        private Link LinkEmEdicao { get; set; }
+
         public Links() {
             InitializeComponent();
             CarregarLinks();
         }
 
         private void btnSalvar_Click(object sender, EventArgs e) {
-            Link link = new Link() { Descricao = txtDescricao.Text, Endereco = txtLink.Text };
-            GerenciadorLinks.AdicionarLinks(link);
-            txtDescricao.Text = "";
-            txtLink.Text = "";
+            if (LinkEmEdicao == null) {
+                Link link = new Link() { Descricao = txtDescricao.Text, Endereco = txtLink.Text };
+                GerenciadorLinks.AdicionarLinks(link);
+            } else {
+                LinkEmEdicao.Descricao = txtDescricao.Text;
+                LinkEmEdicao.Endereco = txtLink.Text;
+            }
+
+            txtDescricao.Text = Texto.TXT_DESCRICAO;
+            txtLink.Text = Texto.TXT_LINK;
             CarregarLinks();
         }
 
@@ -35,13 +44,13 @@ namespace Certweb {
                 pn.Size = new Size(680, 400);
                 pn.AutoSize = true;
 
-                var descricao = new Label() { Text = link.Descricao, Font = new Font("Century Gothic", 8), ForeColor = Color.FromArgb(68, 68, 68) };
+                var descricao = new Label() { Text = link.Descricao, Font = new Font(Texto.FONT, 8), ForeColor = Color.FromArgb(68, 68, 68) };
                 //
-                var endereco = new Label() { Text = link.Endereco, Font = new Font("Century Gothic", 8), ForeColor = Color.FromArgb(134, 134, 134) };
+                var endereco = new Label() { Text = link.Endereco, Font = new Font(Texto.FONT, 8), ForeColor = Color.FromArgb(134, 134, 134) };
                 //
-                var btnEditar = new LinkLabel() { Text = "Editar", Font = new Font("Century Gothic", 8) };
+                var btnEditar = new LinkLabel() { Text = "Editar", Font = new Font(Texto.FONT, 8) };
                 //
-                var btnExcluir = new LinkLabel() { Text = "Excluir", Font = new Font("Century Gothic", 8) };
+                var btnExcluir = new LinkLabel() { Text = "Excluir", Font = new Font(Texto.FONT, 8) };
                 // 
 
                 pn.Controls.Add(descricao);
@@ -62,7 +71,9 @@ namespace Certweb {
         }
 
         private void EditarAction(Link link) {
-
+            LinkEmEdicao = link;
+            txtDescricao.Text = link.Descricao;
+            txtLink.Text = link.Endereco;
         }
 
         private void ExcluirAction(Link link) {
@@ -73,32 +84,32 @@ namespace Certweb {
 
         private void txtDescricao_Enter(object sender, EventArgs e) {
             var campo = (TextBox)sender;
-            if(campo.Text == "Descrição") {
-                campo.Text = "";
+            if(campo.Text == Texto.TXT_DESCRICAO) {
+                campo.Text = String.Empty;
                 campo.ForeColor = Color.Black;
             }
         }
 
         private void txtDescricao_Leave(object sender, EventArgs e) {
             var campo = (TextBox)sender;
-            if (campo.Text == "") {
-                campo.Text = "Descrição";
+            if (campo.Text == String.Empty) {
+                campo.Text = Texto.TXT_DESCRICAO;
                 campo.ForeColor = Color.FromArgb(68, 68, 68);
             }
         }
 
         private void txtLink_Enter(object sender, EventArgs e) {
             var campo = (TextBox)sender;
-            if (campo.Text == "URL do site") {
-                campo.Text = "";
+            if (campo.Text == Texto.TXT_LINK) {
+                campo.Text = String.Empty;
                 campo.ForeColor = Color.Black;
             }
         }
 
         private void txtLink_Leave(object sender, EventArgs e) {
             var campo = (TextBox)sender;
-            if (campo.Text == "") {
-                campo.Text = "URL do site";
+            if (campo.Text == String.Empty) {
+                campo.Text = Texto.TXT_LINK;
                 campo.ForeColor = Color.FromArgb(68, 68, 68);
             }
         }
